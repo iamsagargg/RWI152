479b78586ebc42cb3b769a9e12834b79d6394b7b
iamsagargg <ghritlaharesagar0226@gmail.com>
2023-10-11 11:50:11 +0530

    first commit

---
+++ Todaystask.java
@@ -0,0 +1,12 @@
+public class Todaystask {
+
+	public static void main(String[] args) {
+		String a = "Railworld";
+		String lower = a.toLowerCase();
+		String upper = a.toUpperCase();
+		int len = a.length();
+		String sub = a.substring(1, 5);
+		String con = a.concat(" India");
+		System.out.println(lower+"\n"+upper+"\n"+len+"\n"+sub+"\n"+con);
+	}
+}       
